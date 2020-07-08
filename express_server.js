3293720aceb54e6a1d490697a8e30172a5178225
William <mack.mindenhall@gmail.com>
2020-07-08 21:25:58 +0000

    Fixed error handles on routes to direct to the correct page for edge cases.

--- express_server.js
+++ express_server.js
@@ -3,10 +3,13 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const cookieSession = require("cookie-session");
+
 const {
   generateRandomString,
   findUserByEmail,
   urlsForUser,
+  createCurrentDate,
+  checkForExistingShortURL,
 } = require("./helpers");
 
 const app = express();
@@ -23,8 +26,16 @@ app.use(
 );
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "jonSnow" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "NicolasCageSupreme" },
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "jonSnow",
+    dateCreated: "05/11/2017",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "NicolasCageSupreme",
+    dateCreated: "05/05/2020",
+  },
 };
 
 const users = {
@@ -41,67 +52,12 @@ const users = {
   },
 };
 
-app.get("/", (req, res) => {
-  res.redirect("/urls");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const user = users[req.session.user_id];
-  let templateVars = {};
-  if (user === undefined) {
-    res.redirect("/urls");
-    return;
-  }
-
-  if (urlDatabase[req.params.shortURL] === undefined) {
-    templateVars = { user: undefined, error: `Please enter a valid shortURL` };
-  } else {
-    templateVars = {
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL,
-      user: req.session.user_id,
-      error: null,
-    };
-  }
-
-  res.render("urls_show", templateVars);
-});
-
-//  Url page
-app.get("/urls", (req, res) => {
-  const user = users[req.session.user_id];
-  let templateVars = {};
-
-  if (user === undefined) {
-    templateVars = { urls: {}, user };
-    res.render("urls_index", templateVars);
-    return;
-  }
-
-  const filteredURLs = urlsForUser(user.id, urlDatabase);
-
-  templateVars = { urls: filteredURLs, user };
-
-  res.render("urls_index", templateVars);
-});
-
-app.get("/register", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id],
-  };
-  res.render("register", templateVars);
-});
-
-app.get("/login", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id],
-  };
-  res.render("login", templateVars);
-});
+// GET Requests
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     user: users[req.session.user_id],
+    created: createCurrentDate(),
   };
 
   if (templateVars.user === undefined) {
@@ -111,13 +67,100 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const user = users[req.session.user_id];
+  let templateVars = {};
+  if (user === undefined) {
+    res.redirect("/urls");
+    return;
+  } else if (
+    urlsForUser(req.session.user_id, urlDatabase).hasOwnProperty(
+      req.params.shortURL
+    ) === false
+  ) {
+    templateVars = {
+      user: user,
+      error: "Please enter a valid shortURL.",
+    };
+    res.render("urls_index", templateVars);
+    return;
+  }
+
+  templateVars = checkForExistingShortURL(
+    req.params.shortURL,
+    urlDatabase,
+    req.session.user_id
+  );
+
+  console.log("templateVars: ", templateVars);
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const user = users[req.session.user_id];
+  let templateVars = {};
+
+  if (user === undefined) {
+    templateVars = { urls: {}, user, error: null };
+    res.render("urls_index", templateVars);
+    return;
+  }
+
+  const filteredURLs = urlsForUser(user.id, urlDatabase);
+  templateVars = { urls: filteredURLs, user, error: null };
+
+  res.render("urls_index", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  if (req.session.user_id !== undefined) {
+    res.redirect("/urls");
+    return;
+  }
+
+  let templateVars = {
+    user: users[req.session.user_id],
+  };
+  res.render("register", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  if (req.session.user_id !== undefined) {
+    res.redirect("/urls");
+    return;
+  }
+  let templateVars = {
+    user: users[req.session.user_id],
+  };
+  res.render("login", templateVars);
+});
+
 app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL] === undefined) {
+    let templateVars = {
+      user: undefined,
+      error: "Please enter a valid shortURL.",
+    };
+    res.render("urls_index", templateVars);
+    return;
+  }
   res.redirect(urlDatabase[req.params.shortURL].longURL);
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+// POST Requests
+
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString(6);
-  urlDatabase[newShortURL] = req.body.longURL;
+  urlDatabase[newShortURL] = {
+    userID: req.session.user_id,
+    longURL: req.body.longURL,
+    dateCreated: createCurrentDate(),
+  };
+
   res.redirect(`/urls/${newShortURL}`);
 });
 
--- helpers.js
+++ helpers.js
@@ -2,6 +2,11 @@ const generateRandomString = (length) => {
   return Math.random().toString(36).substr(2, length);
 };
 
+const createCurrentDate = () => {
+  let currentDate = new Date();
+  return `${currentDate.getMonth()}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+};
+
 const findUserByEmail = (providedEmail, database) => {
   for (let user in database) {
     if (database[user].email === providedEmail) {
@@ -16,11 +21,37 @@ const urlsForUser = (id, database) => {
 
   Object.keys(database).forEach((url) => {
     if (database[url].userID === id) {
-      filteredURLs[url] = database[url].longURL;
+      filteredURLs[url] = {
+        longURL: database[url].longURL,
+        created: database[url].dateCreated,
+      };
     }
   });
 
   return filteredURLs;
 };
 
-module.exports = { generateRandomString, findUserByEmail, urlsForUser };
+const checkForExistingShortURL = (shortURL, database, id) => {
+  let templateVars = {};
+
+  if (database[shortURL] === undefined) {
+    templateVars = { user: undefined, error: `Please enter a valid shortURL` };
+  } else {
+    templateVars = {
+      shortURL: shortURL,
+      longURL: database[shortURL].longURL,
+      user: id,
+      created: database[shortURL].dateCreated,
+      error: null,
+    };
+  }
+
+  return templateVars;
+};
+module.exports = {
+  generateRandomString,
+  findUserByEmail,
+  urlsForUser,
+  createCurrentDate,
+  checkForExistingShortURL,
+};
